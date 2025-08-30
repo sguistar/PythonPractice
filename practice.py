de5f9b154e933e11e86836a2ee466151fa1c6244
SeanLin <seanlinxhr@gmail.com>
2025-08-30 20:43:17 +0800

    update the file

--- practice.py
+++ practice.py
@@ -1,19 +1,41 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
-device = torch.device('mps')
-# 为了方便起见，我们定义了一个计算卷积层的函数。
-# 此函数初始化卷积层权重，并对输入和输出提高和缩减相应的维数
-def comp_conv2d(conv2d, X):
-    # 这里的（1，1）表示批量大小和通道数都是1
-    X = X.reshape((1, 1) + X.shape)
-    Y = conv2d(X)
-    # 省略前两个维度：批量大小和通道
-    return Y.reshape(Y.shape[2:])
+from tools import corr2d
+
+def corr2d_multi_in(X, K):
+    # 先遍历“X”和“K”的第0个维度（通道维度），再把它们加在一起
+    return sum(corr2d(x, k) for x, k in zip(X, K))
+
+def corr2d_multi_in_out(X, K):
+    # 迭代“K”的第0个维度，每次都对输入“X”执行互相关运算。
+    # 最后将所有结果都叠加在一起
+    return torch.stack([corr2d_multi_in(X, k) for k in K], 0)
+
+def corr2d_multi_in_out_1x1(X, K):
+    c_i, h, w = X.shape
+    c_o = K.shape[0]
+    X = X.reshape((c_i, h * w))
+    K = K.reshape((c_o, c_i))
+    # 全连接层中的矩阵乘法
+    Y = torch.matmul(K, X)
+    return Y.reshape((c_o, h, w))
 
 
-X = torch.rand(8,8)
-#conv2d1 = nn.Conv2d(1, 1, kernel_size=(3, 5), padding=(0, 1), stride=(3, 4))
-conv2d2 = nn.Conv2d(1, 1, kernel_size=(3, 2), padding=(2, 1))
-#print(comp_conv2d(conv2d1, X).shape) #(8-3+0+3)/3=2, (8-5+2x1+4)/4=2 => output size = (2,2)
-print(comp_conv2d(conv2d2, X).shape) #(8+2x2-(3-1)-1)/1+1=10, (8+2x1-(2-1)-1)/1+1=9
+
+
+X = torch.tensor([[[0.0, 1.0, 2.0], [3.0, 4.0, 5.0], [6.0, 7.0, 8.0]],
+               [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0]]])
+K = torch.tensor([[[0.0, 1.0], [2.0, 3.0]], [[1.0, 2.0], [3.0, 4.0]]])
+
+K = torch.stack((K, K + 1, K + 2), 0)
+
+X = torch.normal(0, 1, (3, 3, 3))
+K = torch.normal(0, 1, (2, 3, 1, 1))
+
+Y1 = corr2d_multi_in_out_1x1(X, K)
+Y2 = corr2d_multi_in_out(X, K)
+assert float(torch.abs(Y1 - Y2).sum()) < 1e-6
+
+print(K.shape)
+print(corr2d_multi_in_out(X, K))
---
+++ tools.py
@@ -0,0 +1,9 @@
+import torch
+def corr2d(X, K):  #@save
+    """计算二维互相关运算"""
+    h, w = K.shape
+    Y = torch.zeros((X.shape[0] - h + 1, X.shape[1] - w + 1))
+    for i in range(Y.shape[0]):
+        for j in range(Y.shape[1]):
+            Y[i, j] = (X[i:i + h, j:j + w] * K).sum()
+    return Y
