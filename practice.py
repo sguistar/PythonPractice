0fe2affedd273ddd760f46a53f6c7a28dfd0ff12
SeanLin <seanlinxhr@gmail.com>
2025-09-15 12:07:35 +0800

    update the file

--- practice.py
+++ practice.py
@@ -9,61 +9,33 @@ import matplotlib.pyplot as plt
 import d2l
 
 device = torch.device('mps')
-plt.rcParams['figure.figsize'] = (16, 8)
 
 
-class LeNet(nn.Module):
-    def __init__(self):
-        super(LeNet, self).__init__()
-        self.net = nn.Sequential(
-            nn.Conv2d(1, 6, kernel_size=5, padding=2), nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(6, 16, kernel_size=5), nn.Sigmoid(),
-            nn.AvgPool2d(kernel_size=2, stride=2),
-            nn.Flatten(),
-            nn.Linear(16 * 5 * 5, 120), nn.Sigmoid(),
-            nn.Linear(120, 84), nn.Sigmoid(),
-            nn.Linear(84, 10)
-        )
+class Residual(nn.Module):
+    def __init__(self, input_channels, num_channels, use_1x1_conv=False, strides=1):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_channels, num_channels,
+                               kernel_size=3, padding=1, stride=strides)
+        self.conv2 = nn.Conv2d(num_channels, num_channels,
+                               kernel_size=3, padding=1)
+        if use_1x1_conv:
+            self.conv3 = nn.Conv2d(
+                num_channels, num_channels, kernel_size=1, stride=strides)
+        else:
+            self.conv3 = None
+        self.bn1 = nn.BatchNorm2d(num_features=num_channels)
+        self.bn2 = nn.BatchNorm2d(num_features=num_channels)
+
+    def forward(self, X):
+        Y = F.relu(self.bn1(self.conv1(X)))
+        Y = self.bn2(self.conv2(Y))
+        if self.conv3:
+            X = self.conv3(X)
+        Y += X
+        return Y
 
 
-class AlexNet(nn.Module):
-    def __init__(self):
-        super(AlexNet, self).__init__()
-        self.net = nn.Sequential(
-            # 这里使用一个11*11的更大窗口来捕捉对象。
-            # 同时，步幅为4，以减少输出的高度和宽度。
-            # 另外，输出通道的数目远大于LeNet
-            nn.Conv2d(1, 96, kernel_size=11, stride=4), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            # 减小卷积窗口，使用填充为2来使得输入与输出的高和宽一致，且增大输出通道数
-            nn.Conv2d(96, 256, kernel_size=5, padding=2), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            # 使用三个连续的卷积层和较小的卷积窗口。
-            # 除了最后的卷积层，输出通道的数量进一步增加。
-            # 在前两个卷积层之后，汇聚层不用于减少输入的高度和宽度
-            nn.Conv2d(256, 384, kernel_size=3, padding=1), nn.ReLU(),
-            nn.Conv2d(384, 384, kernel_size=3, padding=1), nn.ReLU(),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1), nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Flatten(),
-            # 这里，全连接层的输出数量是LeNet中的好几倍。使用dropout层来减轻过拟合
-            nn.Linear(256 * 5 * 5, 4096), nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 4096), nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(4096, 10), nn.ReLU()
-        )
-
-
-aln = AlexNet()
-net = aln.net
-X = torch.rand(size=(1, 1, 224, 224), dtype=torch.float32)
-for layer in net:
-    X = layer(X)
-    print(layer.__class__.__name__, 'output shape: \t', X.shape)
-
-batch_size = 128
-train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, resize=224)
-lr, num_epochs = 0.01, 30
-d2l.train_ch6(net, train_iter, test_iter, num_epochs, lr, device=device)
+blk = Residual(3, 3)
+X = torch.rand(4, 3, 6, 6)
+Y = blk(X)
+print(Y.shape)
