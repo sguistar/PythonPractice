f708de149979b930727587b537ae5de1db6209ed
SeanLin <seanlinxhr@gmail.com>
2025-09-23 09:23:09 +0800

    update the file

--- homework.py
+++ homework.py
@@ -2,4 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+img1 = cv2.imread('1.jpg')
+M = np.array([[1,0,100],[0,1,200]])
+img1_t = cv2.warpAffine(img1, M, (img1.shape[1], img1.shape[0]))
 
