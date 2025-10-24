37186a496aed7933831a2b3f82e937f6a1881fe2
Sean Lin <seanlinxhr@gmail.com>
2025-10-24 17:08:44 +0800

    update the file

--- nlp.py
+++ nlp.py
@@ -4,4 +4,4 @@ text = ('您好,请稍等^-^')
 # 词性标注
 seg = psg.cut(text)
 for st, label in enumerate(seg):
-    print(st, label)
+    print(st,label)
--- test.py
+++ test.py
@@ -1,6 +1,9 @@
-import torch
-from torch import nn
-import torchaudio
-import matplotlib.pyplot as plt
-import d2l
-import librosa
+import cv2
+import numpy as np
+
+img = cv2.imread(r'D:\PycharmProjects\class practice\4.jpg')
+cv2.imshow('img', img)
+w = np.array([[0, -1, -1, 0], [-1, 4, 5, -1], [0, -1, -1, 0]])
+img_new = cv2.filter2D(img, -1, w)
+cv2.imshow('img_new', img_new)
+cv2.waitKey(0)
