8871c250e58c6eaf2e800d8424e4fb43d8770e39
Sean Lin <seanlinxhr@gmail.com>
2025-10-27 16:47:59 +0800

    update the file

--- nlp.py
+++ nlp.py
@@ -1,7 +1,8 @@
-import jieba.posseg as psg
+import jieba
+import jieba.analyse as analyse
 
-text = ('您好,请稍等^-^')
-# 词性标注
-seg = psg.cut(text)
-for st, label in enumerate(seg):
-    print(st,label)
+text = '在实践中，当给定相同的查询、键和值的集合时， 我们希望模型可以基于相同的注意力机制学习到不同的行为， 然后将不同的行为作为知识组合起来， 捕获序列内各种范围的依赖关系 （例如，短距离依赖和长距离依赖关系）。 因此，允许注意力机制组合使用查询、键和值的不同 子空间表示（representation subspaces）可能是有益的。'
+seg_list = jieba.cut(text)# 精确模式
+print("Default Mode: " + "/ ".join(seg_list))
+analyse.extract_tags(text,topK=5)
+print("Top 5 keywords: " + ", ".join(analyse.extract_tags(text,topK=5)))
