89a40c5e240b872879b5feee5ba8923db5302a36
Sean Lin <seanlinxhr@gmail.com>
2026-02-04 12:08:11 +0800

    delete the file

--- practice.py
+++ practice.py
@@ -1,23 +1,6 @@
-s = input()
-length = len(s)
-l = 0
-t = ''.join(sorted(s))
-if t == s:
-    print('Yes')
-else:
-    while l < length and s[l] == t[l]:
-        l += 1
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.decomposition import PCA
 
-    r = length - 1
-
-    while r >= 0 and s[r] == t[r]:
-        r -= 1
-
-    s2 = s[:l] + s[l:r + 1][::-1] + s[r + 1:]
-    if s2 == t:
-        print('Yes')
-    else:
-        print('No')
-
-
-        
